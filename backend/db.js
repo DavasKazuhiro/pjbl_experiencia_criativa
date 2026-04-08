18b8dfb751249fba5165373e1515d752daf5bee6
DavasKazuhiro <daviknatume@gmail.com>
2026-04-08 17:09:37 -0300

    fix(main): alteração do .sql para gerar o banco de dados

--- backend/db.js
+++ backend/db.js
@@ -4,5 +4,5 @@ export const db = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "alterar senha aqui",
-    database: "pjbl"
+    database: "pjbl_davi_natume"
 });
--- bd/pjbl_bd.sql
+++ bd/pjbl_bd.sql
@@ -19,6 +19,9 @@
 -- Table structure for table `produtos`
 --
 
+CREATE DATABASE IF NOT EXISTS `pjbl_davi_natume`;
+USE `pjbl_davi_natume`;
+
 DROP TABLE IF EXISTS `produtos`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
